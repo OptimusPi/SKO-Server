d73b3c4b6bab056cc3bf34b40800b250174ea001
Nathanial P. Howard <admin@optimuspi.com>
2018-07-21 17:29:04 -0500

    Fix a crash where clan could not be created because bio cannot be null.
    Fix a crash where clan could not be created because bio cannot be null. Set default value to 'A Stick Knights clan.'

--- schema.sql
+++ schema.sql
@@ -94,7 +94,7 @@ CREATE TABLE IF NOT EXISTS `clan` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `owner_id` int(11) NOT NULL,
   `name` varchar(19) NOT NULL,
-  `bio` varchar(255) NOT NULL,
+  `bio` varchar(255) NOT NULL DEFAULT 'A Stick Knights clan.',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
